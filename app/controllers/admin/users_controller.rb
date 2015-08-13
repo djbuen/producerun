40b97b1a46b1672e8857f2975556a1ff305c9d2e
Giulliano Bueno <giubueno@gmail.com>
2015-08-13 01:02:00 -0300

    If the admin don't specify the number of users per page in the admin area, the system will list all users without pagination.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,10 +6,11 @@ class Admin::UsersController < Admin::BaseController
 
   protected
   def collection
-    if !params[:per_page].nil?
-      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(1).per(params[:per_page])
+    @total_users ||= User.count
+    if params[:per_page].nil?
+      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(1).per(@total_users)
     else
-      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
+      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page]).per(params[:per_page])
     end
   end
 end
