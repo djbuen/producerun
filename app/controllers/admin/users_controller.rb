af22f7b6b0f2fd752067e15782b74dfb227b7300
Giulliano Bueno <giubueno@gmail.com>
2015-08-13 00:12:24 -0300

    Allow to control the number of users in the result list in the admin area.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,11 @@ class Admin::UsersController < Admin::BaseController
 
   protected
   def collection
-    @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
+    if !params[:per_page].nil?
+      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(1).per(params[:per_page])
+    else
+      @users ||= apply_scopes(end_of_association_chain).with_user_totals.order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
+    end
   end
 end
 
